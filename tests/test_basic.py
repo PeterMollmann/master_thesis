5830981726dc6dd13a08d20bd1b53b3b7799e335
PeterMoellmann <thorhaugemoellmann@gmail.com>
2025-03-31 14:32:55 +0200

    Making basic test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,2 +1,10 @@
+from GetDataFromScratch.GetTopographyData import GetTopographyData
+from GetDataFromScratch.DataLoader import DataLoader
+
+
 def test_example():
-    assert 1 + 1 == 2
+    # assert 1 + 1 == 2
+    coords = DataLoader(fileNameID="SY600_n01", toLoad="coords")
+    x, y, z = coords
+    h_r, w, h_p = GetTopographyData(x, y, z, lowerBound=1.5, upperBound=2.0)
+    print(h_r, w, h_p)
