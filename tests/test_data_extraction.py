be59e1189430a8ee91c1f662a5635714840daf86
PeterMoellmann <thorhaugemoellmann@gmail.com>
2025-03-31 14:48:30 +0200

    renamed and finished first test

--- tests/test_data_extraction.py
+++ tests/test_data_extraction.py
@@ -3,8 +3,15 @@ from GetDataFromScratch.DataLoader import DataLoader
 
 
 def test_example():
-    # assert 1 + 1 == 2
     coords = DataLoader(fileNameID="SY600_n01", toLoad="coords")
-    x, y, z = coords
+    # print(coords)
+    x, y, z = coords[:, 0], coords[:, 1], coords[:, 2]
     h_r, w, h_p = GetTopographyData(x, y, z, lowerBound=1.5, upperBound=2.0)
-    print(h_r, w, h_p)
+    # print(h_r, w, h_p)
+    assert h_r < 0.5
+    assert w < 0.5
+    assert h_p < 0.5
+
+
+if __name__ == "__main__":
+    test_example()
