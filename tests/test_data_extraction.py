fa3081faf36394720521e3a3ca074977c28f42f5
PeterMoellmann <thorhaugemoellmann@gmail.com>
2025-04-07 09:48:12 +0200

    updated to work with new GetTopographyData

--- tests/test_data_extraction.py
+++ tests/test_data_extraction.py
@@ -5,10 +5,10 @@ def test_data_extraction():
     dataPath = "src/GetDataFromScratch/ScratchData/TestData/"
     coords = LoadScratchTestData(
         fileNameID="SY600_n01", path=dataPath, toLoad="coords")
-    h_r, w, h_p = GetTopographyData(coords, lowerBound=1.5, upperBound=2.0)
+    h_r, w, h_p = GetTopographyData(coords, lowerBound=2.00, upperBound=2.44)
     print(h_r, w, h_p)
-    assert round(h_r, 3) == 0.054
-    assert round(w, 3) == 0.376
+    assert round(h_r, 3) == 0.042
+    assert round(w, 3) == 0.374
     assert round(h_p, 3) == 0.025
 
 
