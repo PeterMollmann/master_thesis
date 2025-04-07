e7f90a167f47e2823e51b4bb4ada5613b036ec26
PeterMoellmann <thorhaugemoellmann@gmail.com>
2025-04-07 09:48:25 +0200

    Made simple sr test work

--- tests/test_simple_sr.py
+++ tests/test_simple_sr.py
@@ -1,5 +1,7 @@
 import numpy as np
 from SRScratch.SRScratch import SRScratch
+from alpine.data import Dataset
+
 import os
 import yaml
 
@@ -7,7 +9,7 @@ import yaml
 def test_simple_sr():
 
     x = np.array([x/10. for x in range(-10, 10)])
-    y = 2*x**3 + 1.2*x**2 + 4.1*x
+    y = x**3 + x**2 + x
 
     yamlPath = "test_simple_sr.yaml"
     yamlfile = yamlPath
@@ -15,9 +17,15 @@ def test_simple_sr():
     with open(filename) as config_file:
         config_file_data = yaml.safe_load(config_file)
 
-    best_ind = SRScratch(config_file_data=config_file_data,
-                         features=x, target=y)
-    print(best_ind)
+    train_data = Dataset("dataset", x, y)
+    train_data.X = [train_data.X]
+
+    seed = [
+        "add(add(add(mul(mul(x, mul(x, x)),x), mul(x,mul(x, x))), mul(x, x)), x)"]
+
+    _, fit_score = SRScratch(config_file_data=config_file_data,
+                             trainDataSet=train_data, seed=seed)
+    assert fit_score <= 1e-12
 
 
 if __name__ == "__main__":
