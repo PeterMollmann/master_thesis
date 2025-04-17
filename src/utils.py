a7c61bc0d64861c91be9c69e50f91daa054f479d
PeterMoellmann <thorhaugemoellmann@gmail.com>
2025-04-17 11:36:16 +0200

    added utils

---
+++ src/utils.py
@@ -0,0 +1,7 @@
+
+def printMethodInfo(method_name, best_params, r2_score):
+    print("="*50)
+    print(f"Method: {method_name}")
+    print(f"Best Parameters: {best_params}")
+    print(f"R^2 Score: {r2_score}")
+    print("="*50)
