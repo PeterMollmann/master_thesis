c1881791068c962db465c494614d96d92cc649cb
PeterMoellmann <thorhaugemoellmann@gmail.com>
2025-04-18 15:18:14 +0200

    small changes

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 from simpleRegression.utils import printMethodInfo, plot_data
 from simpleRegression.fitFunctions import SimpleRegression
 
 
-def main():
+def main(regressionMethod, use_interactions, n_features, target_data):
 
     # Load Norm data
     NormData = pd.read_csv(
@@ -13,14 +12,14 @@ def main():
     )
 
     # Extract data
-    features = np.array([NormData["syNorm"], NormData["n"], NormData["wNorm"]])
-    # features = np.array([NormData["syNorm"], NormData["n"]])
-    target = NormData["fnNorm"]
+    if n_features == 2:
+        features = np.array([NormData["syNorm"], NormData["n"]])
+    elif n_features == 3:
+        features = np.array([NormData["syNorm"], NormData["n"], NormData["wNorm"]])
+
+    target = NormData[target_data]
 
     # Perform regression
-    regressionMethod = "secondOrder"
-    use_interactions = True
-
     if regressionMethod == "linear":
         degree = 1
     elif regressionMethod == "secondOrder":
@@ -46,4 +45,9 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    main(
+        regressionMethod="secondOrder",
+        use_interactions=False,
+        n_features=2,
+        target_data="fnNorm",
+    )
