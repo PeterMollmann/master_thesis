63e29c800ae8e4a7963f9f80e27af4f5cef62539
PeterMoellmann <thorhaugemoellmann@gmail.com>
2025-04-18 15:28:33 +0200

    fixed test because of changes to fitFunctions

--- tests/test_linear_regression.py
+++ tests/test_linear_regression.py
@@ -1,5 +1,5 @@
 import numpy as np
-from simpleRegression.fitFunctions import linearFit
+from simpleRegression.fitFunctions import SimpleRegression
 from scipy.optimize import curve_fit
 
 
@@ -7,8 +7,13 @@ def test_linear_regression():
     # Example data
     x = np.array([1, 2, 3, 4, 5])
     y = np.array([2, 3, 5, 7, 11])
-    z = 2 + 3*x + 4*y
+    z = 2 + 3 * x + 4 * y
     # Fit the data
-    popt, pcov = curve_fit(linearFit, (x, y), z, p0=(1, 1, 1))
+    popt, _, _ = SimpleRegression(
+        features=np.array([x, y]),
+        target=z,
+        degree=1,
+    )
     assert np.allclose(
-        popt, [2, 3, 4], atol=0.1), f"Expected parameters close to [2, 3, 4], got {popt}"
+        popt, [2, 3, 4], atol=0.1
+    ), f"Expected parameters close to [2, 3, 4], got {popt}"
