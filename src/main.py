0c7dc8584f213db0bb4e3b8712eef3a0321d9f78
PeterMoellmann <thorhaugemoellmann@gmail.com>
2025-04-24 11:32:05 +0200

    updated to support train_test_split

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from simpleRegression.utils import printMethodInfo, plot_data
-from simpleRegression.fitFunctions import SimpleRegression
+from simpleRegression.fitFunctions import fittingFunction, SimpleRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_squared_error
 
 
-def main(regressionMethod, use_interactions, n_features, target_data):
+def main(regressionMethod, use_interactions, n_features, target_data, save_fig=False):
 
     # Load Norm data
     NormData = pd.read_csv(
@@ -14,9 +17,21 @@ def main(regressionMethod, use_interactions, n_features, target_data):
     # Extract data
     if n_features == 2:
         features = np.array([NormData["syNorm"], NormData["n"]])
+        xlabel = r"$\sigma_y/E$"
+        ylabel = r"$n$"
     elif n_features == 3:
         features = np.array([NormData["syNorm"], NormData["n"], NormData["wNorm"]])
+        xlabel = r"$\sigma_y/E$"
+        ylabel = r"$n$"
 
+    if target_data == "fnNorm":
+        zlabel = r"$f_n/Ew^2$"
+    elif target_data == "ftNorm":
+        zlabel = r"$f_t/Eh_d\sqrt{wh_d}$"
+    elif target_data == "hrNorm":
+        zlabel = r"$h_r/h_d$"
+    elif target_data == "hpNorm":
+        zlabel = r"$h_p/h_d$"
     target = NormData[target_data]
 
     # Perform regression
@@ -25,29 +40,91 @@ def main(regressionMethod, use_interactions, n_features, target_data):
     elif regressionMethod == "secondOrder":
         degree = 2
 
-    best_params, r2, mse = SimpleRegression(
-        features=features,
-        target=target,
+    # Data train test splitting
+    X_train, X_test, y_train, y_test = train_test_split(
+        features.T, target, test_size=0.2, random_state=42
+    )
+
+    best_params, r2_train, mse_train = SimpleRegression(
+        features=X_train.T,
+        target=y_train,
         degree=degree,
         use_interactions=use_interactions,
     )
 
-    printMethodInfo(regressionMethod, best_params, r2, mse)
+    # Fitted function on test data
+    y_pred = fittingFunction(
+        X_test.T,
+        *best_params,
+        degree=degree,
+        include_interactions=use_interactions,
+    )
+
+    printMethodInfo(
+        method_name=regressionMethod,
+        target_name=target_data,
+        best_params=best_params,
+        n_features=n_features,
+        use_interactions=use_interactions,
+        train_r2_score=r2_train,
+        train_mse=mse_train,
+        test_r2_score=r2_score(y_true=y_test, y_pred=y_pred),
+        test_mse=mean_squared_error(y_true=y_test, y_pred=y_pred),
+    )
 
     # Plotting
     plot_data(
-        features=features,
-        target=target,
+        features_all=features,
+        target_all=target,
+        features_test=X_test.T,
+        target_test=y_test,
         method_name=regressionMethod,
         best_params=best_params,
         include_interactions=use_interactions,
+        xlabel=xlabel,
+        ylabel=ylabel,
+        zlabel=zlabel,
+        target_name=target_data,
+        save_fig=save_fig,
     )
 
 
 if __name__ == "__main__":
-    main(
-        regressionMethod="secondOrder",
-        use_interactions=False,
-        n_features=2,
-        target_data="fnNorm",
-    )
+    loop = False
+
+    if loop:
+        regressionMethods = ["linear", "secondOrder"]
+        use_interactions = [True, False]
+        n_features = [2, 3]
+        target_data = ["fnNorm", "ftNorm", "hrNorm", "hpNorm"]
+
+        for i in regressionMethods:
+            for k in n_features:
+                for l in target_data:
+                    if i == "secondOrder":
+                        for j in use_interactions:
+                            print(i, k, l, j)
+                            main(
+                                regressionMethod=i,
+                                use_interactions=j,
+                                n_features=k,
+                                target_data=l,
+                                save_fig=True,
+                            )
+                    else:
+                        print(i, k, l)
+                        main(
+                            regressionMethod=i,
+                            use_interactions=False,
+                            n_features=k,
+                            target_data=l,
+                            save_fig=True,
+                        )
+    else:
+        main(
+            regressionMethod="secondOrder",
+            use_interactions=False,
+            n_features=2,
+            target_data="hrNorm",
+            save_fig=False,
+        )
--- src/simpleRegression/utils.py
+++ src/simpleRegression/utils.py
@@ -1,41 +1,88 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from simpleRegression.fitFunctions import fittingFunction
+from sklearn.model_selection import train_test_split
 
 
-def printMethodInfo(method_name, best_params, r2_score, mse):
+def printMethodInfo(
+    method_name,
+    target_name,
+    best_params,
+    n_features: int,
+    use_interactions: bool,
+    train_r2_score: float,
+    train_mse: float,
+    test_r2_score: float,
+    test_mse: float,
+):
+    """
+    Print the regression method information, including the fitted function and parameters.
+    Args:
+        method_name (str): The name of the regression method used.
+        best_params (array-like): The optimal parameters from the regression.
+        n_features (int): The number of features used in the regression.
+        use_interactions (bool): Whether interaction terms were included.
+        r2_score (float): The R^2 score of the regression.
+        mse (float): The mean squared error of the regression.
+    """
     print("=" * 50)
     print(f"Method: {method_name}")
+    print(f"Target: {target_name}")
+    printFittedFunction(
+        popt=best_params,
+        n_features=n_features,
+        degree=1 if method_name == "linear" else 2,
+        use_interactions=use_interactions,
+    )
     print(f"Best Parameters: {best_params}")
-    print(f"R^2 Score: {r2_score}")
-    print(f"Mean Squared Error: {mse}")
+    print(f"Train R^2 Score: {train_r2_score}")
+    print(f"Train Mean Squared Error: {train_mse}")
+    print(f"Test R^2 Score: {test_r2_score}")
+    print(f"Test Mean Squared Error: {test_mse}")
     print("=" * 50)
 
 
-def plot_data(features, target, method_name, best_params, include_interactions=True):
+def plot_data(
+    features_all,
+    target_all,
+    features_test,
+    target_test,
+    method_name: str,
+    best_params,
+    include_interactions: bool = True,
+    xlabel: str = "Feature 1",
+    ylabel: str = "Feature 2",
+    zlabel: str = "Target Variable",
+    target_name: str = "Target",
+    save_fig: bool = False,
+):
     """
     Plot the data and the fitted curve based on the regression method.
 
     Args:
         features (tuple): A tuple containing the feature data (x1, x2).
         target (array-like): The target variable data (y).
-        best_params (array-like): The optimal parameters from the regression.
         method_name (str): The name of the regression method used.
+        best_params (array-like): The optimal parameters from the regression.
+        include_interactions (bool): Whether to include interaction terms.
+        xlabel (str): The label for the x-axis.
+        ylabel (str): The label for the y-axis.
+        zlabel (str): The label for the z-axis.
+        target_name (str): The name of the target variable.
+        save_fig (bool): Whether to save the figure as a PNG file.
+
     """
-    if len(features) == 2:
-        x1, x2 = features
-        y = target
+    if len(features_all) == 2:
+        x1, x2 = features_all
+        y = target_all
+        x1_test, x2_test = features_test
+        y_test = target_test
 
-    elif len(features) == 3:
-        x1, x2, x3 = features
-        y = target
-        x3_grid = np.array(x3).reshape(len(np.unique(x1)), len(np.unique(x2)))
-
-    # Create a grid for plotting
-    x1_grid = np.array(x1).reshape(len(np.unique(x1)), len(np.unique(x2)))
-    x2_grid = np.array(x2).reshape(len(np.unique(x1)), len(np.unique(x2)))
-    y_true_grid = np.array(y).reshape(len(np.unique(x1)), len(np.unique(x2)))
-    y_grid = None
+    elif len(features_all) == 3:
+        x1, x2, x3 = features_all
+        y = target_all
+        x1_test, x2_test, x3_test = features_test
+        y_test = target_test
 
     if method_name == "linear":
         degree = 1
@@ -43,28 +90,143 @@ def plot_data(features, target, method_name, best_params, include_interactions=T
         degree = 2
 
     # Calculate the fitted values based on the regression method
-    y_grid = fittingFunction(
-        features, *best_params, degree=degree, include_interactions=include_interactions
+    y_pred = fittingFunction(
+        features_all,
+        *best_params,
+        degree=degree,
+        include_interactions=include_interactions,
     )
+    y_pred_grid = y_pred.reshape(len(np.unique(x1)), len(np.unique(x2)))
 
-    # Plotting
+    # Create a grid for surface plotting
+    x1_grid = np.array(x1).reshape(len(np.unique(x1)), len(np.unique(x2)))
+    x2_grid = np.array(x2).reshape(len(np.unique(x1)), len(np.unique(x2)))
+    y_grid = np.array(y).reshape(len(np.unique(x1)), len(np.unique(x2)))
+
+    # Scatter plotting train and test data
+    X_train, X_test, y_train, y_test = train_test_split(
+        features_all.T, target_all, test_size=0.2, random_state=42
+    )
+    x1, x2 = X_train[:, 0], X_train[:, 1]
+    x1_test, x2_test = X_test[:, 0], X_test[:, 1]
     fig = plt.figure()
     ax = fig.add_subplot(111, projection="3d")
-    ax.scatter(x1, x2, y, color="r", label="Data Points")
+    ax.scatter(
+        x1,
+        x2,
+        y_train,
+        color="r",
+        linewidths=1,
+        alpha=1,
+        # edgecolors="k",
+        label="FEM Data Points",
+    )
+    ax.scatter(
+        x1_test,
+        x2_test,
+        y_test,
+        color="k",
+        linewidths=1,
+        alpha=1,
+        # edgecolors="k",
+        label="FEM Data Points for Test",
+    )
+
+    # Surface plotting
     ax.plot_surface(
-        x1_grid, x2_grid, y_true_grid, alpha=0.5, color="g", label="True Surface"
+        x1_grid,
+        x2_grid,
+        y_grid,
+        alpha=0.5,
+        color="g",
+        label="FEM Surface",
     )
     ax.plot_surface(
         x1_grid,
         x2_grid,
-        y_grid.reshape(40, 5),
+        y_pred_grid,
         alpha=0.5,
         color="b",
         label="Fitted Surface",
     )
-    ax.set_xlabel("Feature 1")
-    ax.set_ylabel("Feature 2")
-    ax.set_zlabel("Target Variable")
-    plt.title(f"{method_name} Regression Fit")
-    plt.legend()
-    plt.show()
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_zlabel(zlabel)
+
+    ax.set_xlim([np.min(x1), np.max(x1)])
+    ax.set_ylim([np.min(x2), np.max(x2)])
+    if np.min(y) > np.min(y_pred) and np.max(y) > np.max(y_pred):
+        ax.set_zlim([np.min(y_pred), np.max(y)])
+    elif np.min(y) < np.min(y_pred) and np.max(y) > np.max(y_pred):
+        ax.set_zlim([np.min(y), np.max(y)])
+    elif np.min(y) > np.min(y_pred) and np.max(y) < np.max(y_pred):
+        ax.set_zlim([np.min(y_pred), np.max(y_pred)])
+    elif np.min(y) < np.min(y_pred) and np.max(y) < np.max(y_pred):
+        ax.set_zlim([np.min(y), np.max(y_pred)])
+
+    # plt.title(f"{method_name} Regression Fit")
+    ax.legend(
+        loc="upper center",
+        bbox_to_anchor=(0.5, 1.05),
+        ncol=2,
+        fancybox=True,
+        shadow=True,
+    )
+    # plt.grid()
+
+    if save_fig:
+        plt.savefig(
+            "src/simpleRegression/saved_figs/"
+            + str(target_name)
+            + "_nFeatures"
+            + str(len(features_all))
+            + "_degree"
+            + str(degree)
+            + "_interactions"
+            + str(include_interactions)
+            + ".png"
+        )
+    else:
+        plt.show()
+    plt.close(fig)
+
+
+def printFittedFunction(
+    popt, n_features: int = 2, degree: int = 2, use_interactions: bool = True
+):
+    """
+    Print the fitted function based on the regression method and parameters.
+    Args:
+        popt (array-like): The optimal parameters from the regression.
+        n_features (int): The number of features used in the regression.
+        degree (int): The degree of the polynomial fit.
+        use_interactions (bool): Whether to include interaction terms, e.g. x1*x2.
+    """
+
+    if degree == 1:
+        print("Linear regression")
+        if n_features == 2:
+            print(f"y = {popt[0]} + {popt[1]} * x1 + {popt[2]} * x2")
+        elif n_features == 3:
+            print(f"y = {popt[0]} + {popt[1]} * x1 + {popt[2]} * x2 + {popt[3]} * x3")
+    elif degree == 2:
+        print("Polynomial regression")
+        if use_interactions:
+            print("Including interaction terms")
+            if n_features == 2:
+                print(
+                    f"y = {popt[0]} + {popt[1]} * x1 + {popt[2]} * x2 + {popt[3]} * x1^2 + {popt[4]} * x2^2 + {popt[5]} * x1*x2"
+                )
+            elif n_features == 3:
+                print(
+                    f"y = {popt[0]} + {popt[1]} * x1 + {popt[2]} * x2 + {popt[3]} * x3 + {popt[4]} * x1^2 + {popt[5]} * x2^2 + {popt[6]} * x3^2 + {popt[7]} * x1*x2 + {popt[8]} * x1*x3 + {popt[9]} * x2*x3"
+                )
+        else:
+            if n_features == 2:
+                print(
+                    f"y = {popt[0]} + {popt[1]} * x1 + {popt[2]} * x2 + {popt[3]} * x1^2 + {popt[4]} * x2^2"
+                )
+            elif n_features == 3:
+                print(
+                    f"y = {popt[0]} + {popt[1]} * x1 + {popt[2]} * x2 + {popt[3]} * x3 + {popt[4]} * x1^2 + {popt[5]} * x2^2 + {popt[6]} * x3^2"
+                )
